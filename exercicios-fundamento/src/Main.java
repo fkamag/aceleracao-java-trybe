ad2f32b42f10f68c4aaf291c41f6cf2b554bd382
fkamag <fabiokamagian@gmail.com>
2024-04-19 19:45:09 -0300

    exercicios List

--- exercicios-fundamento/src/Main.java
+++ exercicios-fundamento/src/Main.java
@@ -1,46 +1,55 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
   public static void main(String[] args) {
 
-    // testando SmartHouse
-    SmartHouse smartHouse = new SmartHouse();
-    System.out.println("Estado da lâmpada: " + smartHouse.isLampadaLigada());
-    System.out.println("Ligar lâmpada");
-    smartHouse.ligarLampada();
-    System.out.println("Estado da lâmpada: " + smartHouse.isLampadaLigada());
-    System.out.println("Desligar lâmpada");
-    smartHouse.desligarLampada();
-    System.out.println("Estado da lâmpada: " + smartHouse.isLampadaLigada());
-    System.out.println("ligar internet " + smartHouse.conectarInternet(120));
+//    // testando SmartHouse
+//    SmartHouse smartHouse = new SmartHouse();
+//    System.out.println("Estado da lâmpada: " + smartHouse.isLampadaLigada());
+//    System.out.println("Ligar lâmpada");
+//    smartHouse.ligarLampada();
+//    System.out.println("Estado da lâmpada: " + smartHouse.isLampadaLigada());
+//    System.out.println("Desligar lâmpada");
+//    smartHouse.desligarLampada();
+//    System.out.println("Estado da lâmpada: " + smartHouse.isLampadaLigada());
+//    System.out.println("ligar internet " + smartHouse.conectarInternet(120));
+//
+//    // testando Television
+//    Television tv = new Television();
+//    Television television = new Television("Phillips", "LCD", 32);
+//    television.turnOn();
+//    television.turnOff();
+//
+//    System.out.println(television.info());
+//
+//    System.out.print("O volume atual é: ");
+//    System.out.println(television.getVolume());
+//    System.out.println("Aumentando o volume");
+//    for (int i = 0; i < 8; i++) {
+//      television.increaseVolume();
+//    }
+//    System.out.print("O volume atual é: ");
+//    System.out.println(television.getVolume());
+//    System.out.println("Abaixando o volume");
+//    for (int i = 0; i < 6; i++) {
+//      television.descreaseVolume();
+//    }
+//    System.out.print("O volume atual é: ");
+//    System.out.println(television.getVolume());
+//
+//    System.out.print("Modelo antigo: ");
+//    System.out.println(television.getModel());
+//    television.setModel("LCD-Ultra");
+//    System.out.print("Modelo novo: ");
+//    System.out.println(television.getModel());
 
-    // testando Television
-    Television tv = new Television();
-    Television television = new Television("Phillips", "LCD", 32);
-    television.turnOn();
-    television.turnOff();
+//
 
-    System.out.println(television.info());
 
-    System.out.print("O volume atual é: ");
-    System.out.println(television.getVolume());
-    System.out.println("Aumentando o volume");
-    for (int i = 0; i < 8; i++) {
-      television.increaseVolume();
-    }
-    System.out.print("O volume atual é: ");
-    System.out.println(television.getVolume());
-    System.out.println("Abaixando o volume");
-    for (int i = 0; i < 6; i++) {
-      television.descreaseVolume();
-    }
-    System.out.print("O volume atual é: ");
-    System.out.println(television.getVolume());
-
-    System.out.print("Modelo antigo: ");
-    System.out.println(television.getModel());
-    television.setModel("LCD-Ultra");
-    System.out.print("Modelo novo: ");
-    System.out.println(television.getModel());
   }
 
 }
+
