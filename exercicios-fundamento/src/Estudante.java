9da699f887dcf247f5a5a83ee2412d4e9aa7c986
fkamag <fabiokamagian@gmail.com>
2024-04-19 21:57:57 -0300

    exercicio Set

---
+++ exercicios-fundamento/src/Estudante.java
@@ -0,0 +1,23 @@
+class Estudante {
+
+  private String matricula;
+
+  private String nome;
+
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Estudante other = (Estudante) obj;
+    if (matricula == null) {
+      if (other.matricula != null)
+        return false;
+    } else if (!matricula.equals(other.matricula))
+      return false;
+    return true;
+  }
+
+}
--- exercicios-fundamento/src/Main.java
+++ exercicios-fundamento/src/Main.java
@@ -1,6 +1,5 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
 
@@ -44,10 +43,23 @@ public class Main {
 //    System.out.println(television.getModel());
 //    television.setModel("LCD-Ultra");
 //    System.out.print("Modelo novo: ");
-//    System.out.println(television.getModel());
+//    System.out.println(television.getModel())
 
-//
+//    Set<String> estudantes = Set.of("João","Maria","Pedro","Rosa");
+//    System.out.println(estudantes);
+//    estudantes.add("Fábio");  //não pode mudar estudantes pois Set é imutável
 
+    HashSet<String> estudantes = new HashSet<>();
+    boolean adicionouJoao = estudantes.add("João");
+    boolean adicionouJoaoDeNovo = estudantes.add("João");
+    boolean adicionouMaria = estudantes.add("Maria");
+    System.out.printf("adicionouJoao: %b%n",adicionouJoao);
+    System.out.printf("adicionouJoaoDeNovo: %b%n",adicionouJoaoDeNovo);
+    System.out.printf("adicionouMaria: %b%n",adicionouMaria);
+    System.out.println(estudantes);
+    estudantes.remove("João");
+    System.out.println(estudantes.size());
+    System.out.println(estudantes.contains("Maria"));
 
   }
 
